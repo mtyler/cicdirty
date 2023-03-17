482c054dfeba9608bcb98fb964039d1401804e31
mtyler <michael.tyler@gmail.com>
2023-03-17 19:31:04 -0400

    UI updates

--- app/server.js
+++ app/server.js
@@ -10,7 +10,7 @@ console.log( process.env );
 // App
 const app = express();
 app.get('/', (req, res) => {
-  var message = `<p>Build Number/Tag: ${process.env.BUILD}<br><br><a href='http://20.169.220.3:8080/'>Jenkins Server<a/><br><br>QA Service: <a href='${process.env.QA}'>${process.env.QA}<a/><br><br>Production Service: <a href='${process.env.PROD}'>${process.env.PROD}<a/><br>`
+  var message = `<p>Build Tag: ${process.env.BUILD}<br><br><a href='http://20.169.220.3:8080/'>Jenkins Server<a/><br><br>QA Service: <a href='http://${process.env.QA}'>${process.env.QA}<a/><br><br>Production Service: <a href='http://${process.env.PROD}'>${process.env.PROD}<a/><br>`
   res.status(200).send(message);
 });
 
