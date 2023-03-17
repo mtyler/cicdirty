846002ecc2a080dbad5d9fbde36025416235ed0d
mtyler <michael.tyler@gmail.com>
2023-03-17 12:41:33 -0400

    add environment details

--- app/server.js
+++ app/server.js
@@ -9,7 +9,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World\n\n' /
+           'This is build number: ' + process.env.BUILD + '\n\n' /
+           'Jenkins server: http://20.169.220.3:8080/\n\n' /
+           '  Prod url:\n\n' /
+           '    QA url:\n\n');
 });
 
 app.listen(PORT, HOST, () => {
