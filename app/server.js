539e9a28eb1e3ffc263ec9ee350d116f399c88b1
mtyler <michael.tyler@gmail.com>
2023-03-22 15:39:33 -0400

    get healthz endpoints

--- app/server.js
+++ app/server.js
@@ -12,8 +12,10 @@ const app = express();
 app.get('/', (req, res) => {
   var message = `<p>BBW Proof of Concept<p><p>Dashboard Version: ${process.env.BUILD}<br> \
                  <br>Jenkins Server: <a href='http://20.169.220.3:8080/blue/organizations/jenkins/bbw-azure-poc/branches'>http://20.169.220.3:8080/<a/><br> \
-                 <br>QA Service: <a href='http://${process.env.QA}'>${process.env.QA}<a/><br> \
-                 <br>Production Service: <a href='http://${process.env.PROD}'>${process.env.PROD}<a/><br>`
+                 <br>QA Service:<br>IP: <a href='http://${process.env.QA}'>${process.env.QA}<a/><br> \
+                 <br>Build: <a href='http://${process.env.QA}/healthz'>/healthz<a/><br> \
+                 <br>Production Service:<br>IP:<a href='http://${process.env.PROD}'>${process.env.PROD}<a/><br> \
+                 <br>Build: <a href='http://${process.env.QA}/healthz'>/healthz<a/><br>`
   res.status(200).send(message);
 });
 
